17d13958e3b92c7013d42a311bda985a766f67c6
Kostya <71220034+kkkkkostya@users.noreply.github.com>
2024-03-17 16:02:39 +0300

    Add files via upload

---
+++ idz_os_1/program6.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUFFER_SIZE 5000
+
+// Функция для чтения данных из файла и передачи
+void read_from_file_and_send(int fd_out, char *input_file) {
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    int fd_in = open(input_file, O_RDONLY);
+    if (fd_in == -1) {
+        perror("Failed to open input file");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((bytes_read = read(fd_in, buffer, BUFFER_SIZE)) > 0) {
+        if (write(fd_out, buffer, bytes_read) != bytes_read) {
+            perror("Write error");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    close(fd_in);
+    close(fd_out);
+}
+
+// Функция для обработки данных и передачи результата
+void process_data_and_send(int fd_in, int fd_out, int start_index, int end_index) {
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd_in, buffer, BUFFER_SIZE)) > 0) {
+        if (bytes_read < end_index) {
+            end_index = bytes_read;
+        }
+        for (int i = start_index; i < (start_index + end_index) / 2; i++) {
+            char temp = buffer[i];
+            buffer[i] = buffer[start_index + end_index - 1 - i];
+            buffer[start_index + end_index - 1 - i] = temp;
+        }
+
+        if (write(fd_out, buffer, bytes_read) != bytes_read) {
+            perror("Write error");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    close(fd_in);
+    close(fd_out);
+}
+
+// Функция для записи данных в файл
+void write_to_file_and_close(int fd_in, char *output_file) {
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    int fd_out = open(output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd_out == -1) {
+        perror("Failed to open output file");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((bytes_read = read(fd_in, buffer, BUFFER_SIZE)) > 0) {
+        if (write(fd_out, buffer, bytes_read) != bytes_read) {
+            perror("Write error");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    close(fd_out);
+    close(fd_in);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 5) {
+        fprintf(stderr, "Usage: %s input_file output_file start_index end_index\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    char *input_file = argv[1];
+    char *output_file = argv[2];
+    int start_index = atoi(argv[3]);
+    int end_index = atoi(argv[4]);
+
+    int pipe1[2], pipe2[2];
+    if (pipe(pipe1) == -1 || pipe(pipe2) == -1) {
+        perror("Pipe error");
+        exit(EXIT_FAILURE);
+    }
+
+
+    close(pipe1[0]); close(pipe2[0]); close(pipe2[1]);
+    read_from_file_and_send(pipe1[1], input_file);
+
+    pid_t pid2 = fork();
+    if (pid2 == -1) {
+        perror("Fork error");
+        exit(EXIT_FAILURE);
+    } else if (pid2 == 0) {
+        // Child process 1
+        close(pipe1[1]); close(pipe2[0]);
+        process_data_and_send(pipe1[0], pipe2[1], start_index, end_index);
+        exit(EXIT_SUCCESS);
+    }
+
+    close(pipe1[0]); close(pipe1[1]); close(pipe2[1]);
+    write_to_file_and_close(pipe2[0], output_file);
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
+    return 0;
+}
